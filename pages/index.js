f909d275ca5eb2d2fdd1a16eed93031b19699205
Marcin Cholewka <marcin@marcincholewka.pl>
2019-01-08 21:43:24 +0100

    Add bootstrap, fonts and App Cleanup.

--- pages/index.js
+++ pages/index.js
@@ -1,47 +1,13 @@
 import React from 'react';
 import BaseLayout from '../components/layouts/BaseLayout';
-import axios from 'axios';
+
+import { Container, Button } from 'reactstrap';
 
 class Index extends React.Component {
-  static async getInitialProps() {
-    let userData = {};
-    try {
-      const response = await axios.get(
-        'https://jsonplaceholder.typicode.com/todos/1'
-      );
-      userData = response.data;
-    } catch (error) {
-      console.log(error);
-    }
-
-    return { userData };
-  }
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      title: `I'm a state title`
-    };
-  }
-
-  componentDidMount() {}
-
-  componentDidUpdate() {}
-
-  componentWillUnmount() {}
-
-  updatedTitle = () => {
-    this.setState({ title: `You changed me` });
-  };
-
   render() {
-    const { title } = this.state;
-
     return (
       <BaseLayout>
-        <h1>I'm Home (index) page</h1>
-        <h2>{title}</h2>
-        <button onClick={this.updatedTitle}>Change title</button>
+        <Container />
       </BaseLayout>
     );
   }
