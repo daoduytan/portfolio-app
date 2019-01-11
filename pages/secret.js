b18e81471b4e827ae52060b4d5f4dc034eb50f2d
Marcin Cholewka <marcin@marcincholewka.pl>
2019-01-11 13:57:16 +0100

    Add secret endpoint server.

--- pages/secret.js
+++ pages/secret.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import BaseLayout from '../components/layouts/BaseLayout';
 import BasePage from '../components/shared/BasePage';
 import withAuth from '../components/hoc/withAuth';
+import axios from 'axios';
 
 class Secret extends Component {
   static getInitialProps() {
@@ -9,6 +10,35 @@ class Secret extends Component {
 
     return { superSecretValue };
   }
+
+  constructor(props) {
+    super();
+    this.state = {
+      secretData: []
+    };
+  }
+
+  async componentDidMount() {
+    const response = await axios.get('/api/v1/secret');
+    const secretData = response.data;
+    this.setState({
+      secretData
+    });
+  }
+
+  displaySecretData = () => {
+    const { secretData } = this.state;
+    return secretData && secretData.length > 0
+      ? secretData.map((data, index) => {
+          return (
+            <div key={index}>
+              <p>{data.title}</p>
+              <p>{data.description}</p>
+            </div>
+          );
+        })
+      : null;
+  };
 
   render() {
     const { superSecretValue } = this.props;
@@ -18,6 +48,7 @@ class Secret extends Component {
           <h1>I'm Secret page</h1>
           <p>Secret content</p>
           <h2>{superSecretValue}</h2>
+          {this.displaySecretData()}
         </BasePage>
       </BaseLayout>
     );
